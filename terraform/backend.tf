f15cb50d30c19ddfad8f1eab3cc3f4b72c22660a
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-07-29 12:43:00 +0530

    latest changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "dhoni-demo-terraform-bucket-123456"
-    key            = "hub-vpc/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    
+    bucket       = "dhoni-demo-terraform-bucket-123456"
+    key          = "hub-vpc/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
   }
 }      
